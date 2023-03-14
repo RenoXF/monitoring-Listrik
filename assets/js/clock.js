a87a5a32152fb23060a99af911098eee896d2aa9
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-14 08:33:33 +0700

    Add clock.js file to assets/js to display current time

---
+++ assets/js/clock.js
@@ -0,0 +1,15 @@
+function updateClock() {
+    console.log('update')
+    let now = new Date();
+    let jam = now.getHours();
+    let menit = now.getMinutes();
+    let detik = now.getSeconds();
+    jam = jam < 10 ? '0' + jam : jam;
+    menit = menit < 10 ? '0' + menit : menit;
+    detik = detik < 10 ? '0' + detik : detik;
+    let waktu = jam + ':' + menit + ':' + detik;
+    document.getElementById('time').innerHTML = waktu;
+    setInterval(updateClock, 1000);
+}
+
+updateClock()
