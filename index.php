fff0ca1a83c336f4667ae0e4c36012486be7b7ff
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-14 08:33:06 +0700

    Add table to display Kompor data

--- index.php
+++ index.php
@@ -2,3 +2,49 @@
 
 define('__IN_SCRIPT__', true);
 
+require_once './includes/header.php'
+
+?>
+<main class="container">
+    <div class="row">
+        <div class="col-12">
+            <div class="table-responsive">
+                <table class="table table-bordered table-hover table-stripped align-middle">
+                    <thead class="text-center">
+                        <tr>
+                            <th>ID Kompor</th>
+                            <th>Voltage</th>
+                            <th>Current</th>
+                            <th>Power</th>
+                            <th>Energy</th>
+                            <th>Frequency</th>
+                            <th>PF</th>
+                            <th>Last Active</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <a href="">1</a>
+                            </td>
+                            <td>
+                                225
+                            </td>
+                            <td>0.12</td>
+                            <td>34.5</td>
+                            <td>2.5</td>
+                            <td>50</td>
+                            <td>0.76</td>
+                            <td class="text-end">
+                                2020-05-01 08:26:36
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</main><?php
+
+require_once './includes/footer.php'
+?>
