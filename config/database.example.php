7df1196ea58daa2ece346f35edbfd4a1b0393750
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-15 10:32:18 +0700

    Add configuration file for database connection

---
+++ config/database.example.php
@@ -0,0 +1,8 @@
+<?php
+
+defined('__IN_SCRIPT__') || exit(1);
+
+define('DB_HOST', 'Your database host');
+define('DB_USER', 'Your database user');
+define('DB_NAME', 'Your database name');
+define('DB_PASS', 'Your database password');
