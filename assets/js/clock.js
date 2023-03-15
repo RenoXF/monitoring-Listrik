0c83469bc300665da1880efea295ad8932c43f23
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-15 10:20:38 +0700

    Update clock.js to use const instead of let for variables

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -1,13 +1,12 @@
 function updateClock() {
-    console.log('update')
-    let now = new Date();
+    const now = new Date();
     let jam = now.getHours();
     let menit = now.getMinutes();
     let detik = now.getSeconds();
     jam = jam < 10 ? '0' + jam : jam;
     menit = menit < 10 ? '0' + menit : menit;
     detik = detik < 10 ? '0' + detik : detik;
-    let waktu = jam + ':' + menit + ':' + detik;
+    const waktu = jam + ':' + menit + ':' + detik;
     document.getElementById('time').innerHTML = waktu;
     setInterval(updateClock, 1000);
 }
