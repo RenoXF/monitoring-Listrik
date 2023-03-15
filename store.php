e07065d92cb68adf9ed063552d7f7ac50ef8a8c1
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-15 11:25:30 +0700

    Add store.php to handle GET requests from the frontend and INSERT data into meter table

---
+++ store.php
@@ -0,0 +1,52 @@
+<?php
+
+define('__IN_SCRIPT__', true);
+
+require './includes/connection.php';
+
+function get($key, $default = null)
+{
+    $val = $_GET[$key] ?? $default;
+
+    $val = htmlspecialchars_decode($val);
+    $val = strip_tags($val);
+
+    return $val;
+}
+
+$idKompor = get('ID_Kompor', 0);
+$date = date('Y-m-d H:i:s');
+$voltage = get('Voltage', 0);
+$current = get('Current', 0);
+$power = get('Power', 0);
+$energy = get('Voltage', 0);
+$frequency = get('Frequency', 0);
+$pf = get('PF', 0);
+
+header("Content-Type: application/json");
+try {
+    $stmt = $mysqli->prepare("INSERT INTO `meter`(`ID_Kompor`, `Date`, `Voltage`, `Current`, `Power`, `Energy`, `Frequency`, `PF`) VALUES (?,?,?,?,?,?,?,?)");
+    $stmt->execute([
+        $idKompor,
+        $date,
+        $voltage,
+        $current,
+        $power,
+        $energy,
+        $frequency,
+        $pf
+    ]);
+    header("HTTP/1.1 200 OK");
+    echo json_encode([
+        'status' => true
+    ]);
+    exit(0);
+} catch (\Throwable $th) {
+    header("HTTP/1.1 500 Server Internal Error");
+    echo json_encode([
+        'status' => false,
+        'code' => $th->getCode(),
+        'message' => $th->getMessage()
+    ]);
+    exit(0);
+}
