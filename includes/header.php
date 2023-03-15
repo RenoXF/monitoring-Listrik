f1d04c58eb0d9670c4f474202c8663b2155a862d
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-15 10:20:47 +0700

    Add Chart.js library to header.php

--- includes/header.php
+++ includes/header.php
@@ -13,6 +13,8 @@ defined('__IN_SCRIPT__') || exit(1);
     <!-- Bootstrap style -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@4.2.0/dist/chart.umd.min.js"></script>
 </head>
 
 <body>
