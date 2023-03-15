45ebc4b13ba2d205b1426fef9dd545b0eb4f4d0d
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-15 10:22:09 +0700

    Update clock JS to use template literals

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -3,10 +3,10 @@ function updateClock() {
     let jam = now.getHours();
     let menit = now.getMinutes();
     let detik = now.getSeconds();
-    jam = jam < 10 ? '0' + jam : jam;
-    menit = menit < 10 ? '0' + menit : menit;
-    detik = detik < 10 ? '0' + detik : detik;
-    const waktu = jam + ':' + menit + ':' + detik;
+    jam = jam < 10 ? `0${jam}` : jam;
+    menit = menit < 10 ? `0${menit}` : menit;
+    detik = detik < 10 ? `0${detik}` : detik;
+    const waktu = `${jam}:${menit}:${detik}`;
     document.getElementById('time').innerHTML = waktu;
     setInterval(updateClock, 1000);
 }
