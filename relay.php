c31a9c5297273c40ea6a423fd101c4a6e4602c49
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-19 19:07:54 +0700

    Add require statements and query to relay.php

--- relay.php
+++ relay.php
@@ -2,4 +2,31 @@
 
 define('__IN_SCRIPT__', true);
 
-echo '1';
+require './includes/connection.php';
+require './helpers/get.php';
+
+
+$idKompor = get('ID_Kompor');
+
+if ($idKompor === null) {
+    exit(0);
+}
+
+$stmt = $mysqli->prepare("SELECT
+    *
+FROM
+    `statusrelay`
+WHERE
+    `ID_Kompor` = ?
+ORDER BY `id` DESC LIMIT 1");
+
+$stmt->execute([$idKompor]);
+
+$result = $stmt->get_result()->fetch_assoc() ?? [];
+
+if (empty($result)) {
+    echo '0';
+    exit(0);
+}
+
+echo $result['Stat'];
