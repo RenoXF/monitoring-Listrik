6ff4c20c5d8402f96c439b5a3634d995164a44e3
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-19 19:25:56 +0700

    Add try/catch block to handle errors when retrieving relay status

--- relay.php
+++ relay.php
@@ -5,28 +5,34 @@ define('__IN_SCRIPT__', true);
 require './includes/connection.php';
 require './helpers/get.php';
 
-
 $idKompor = get('ID_Kompor');
 
 if ($idKompor === null) {
     exit(0);
 }
 
-$stmt = $mysqli->prepare("SELECT
-    *
-FROM
-    `statusrelay`
-WHERE
-    `ID_Kompor` = ?
-ORDER BY `id` DESC LIMIT 1");
+try {
+    $stmt = $mysqli->prepare("SELECT
+        *
+    FROM
+        `statusrelay`
+    WHERE
+        `ID_Kompor` = ?
+    ORDER BY `id` DESC LIMIT 1");
 
-$stmt->execute([$idKompor]);
+    $stmt->execute([$idKompor]);
 
-$result = $stmt->get_result()->fetch_assoc() ?? [];
+    $result = $stmt->get_result()->fetch_assoc() ?? [];
 
-if (empty($result)) {
-    echo '0';
+    if (empty($result)) {
+        $relayStmt = $mysqli->prepare("INSERT INTO `statusrelay` (`ID_Kompor`, `Stat`) VALUES (?, ?)");
+        $relayStmt->execute([$idKompor, 1]);
+        echo '1';
+        exit(0);
+    }
+
+    echo $result['Stat'];
+} catch (\Throwable $th) {
+    echo '1';
     exit(0);
 }
-
-echo $result['Stat'];
